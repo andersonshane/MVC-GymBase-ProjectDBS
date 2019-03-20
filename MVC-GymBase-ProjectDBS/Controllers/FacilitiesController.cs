d228fc26b5bd3dfed91537cb3f3c864bee0d55ca
Shane <Shane@DESKTOP-70TKOKC>
2019-03-20 15:21:04 +0000

    Project update

--- MVC-GymBase-ProjectDBS/Controllers/FacilitiesController.cs
+++ MVC-GymBase-ProjectDBS/Controllers/FacilitiesController.cs
@@ -12,7 +12,51 @@ namespace MVC_GymBase_ProjectDBS.Controllers
 {
     public class FacilitiesController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        static DataSet ds;
+        static DataTable dt;
+
+        public ActionResult Contact()
+        {
+            if (System.IO.File.Exists(Server.MapPath("~/App_Data/facilities.xml")))
+            {
+                ds = new DataSet();
+                ds.ReadXml(Server.MapPath("~/App_Data/facilities.xml"));
+                dt = ds.Tables["user_contact"];
+            }
+            else
+            {
+                ds = new DataSet("contact");
+                dt = new DataTable("user_contact");
+                DataColumn name_col = new DataColumn("name");
+                DataColumn email_col = new DataColumn("email");
+                DataColumn comments_col = new DataColumn("comments");
+                dt.Columns.Add(name_col);
+                dt.Columns.Add(email_col);
+                dt.Columns.Add(comments_col);
+                ds.Tables.Add(dt);
+            }
+            return View(Index());
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string name, string email, string comments)
+        {
+            if (ModelState.IsValid)
+            {
+                DataRow row = dt.NewRow();
+                row["name"] = name;
+                row["email"] = email;
+                row["comments"] = comments;
+                dt.Rows.Add(row);
+                ds.AcceptChanges();
+                ds.WriteXml(Server.MapPath("~/App_Data/facilities.xml"));
+                return View("StatusC");
+            }
+
+            else return View("Index");
+        }
+    
+private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Facilities
         public ActionResult Index()
